82b03c13f08f98fd01d90ed5af4adf1b92722259
Onur Yartaşı <onuryartasi@live.com>
2021-10-13 13:16:28 +0300

    error handling added

--- src/helpers/git.js
+++ src/helpers/git.js
@@ -121,6 +121,9 @@ module.exports = new (class Git {
    */
   push = () => (
     this.exec(`push origin ${branch} --follow-tags`)
+    .catch((err)=>{
+      core.setFaild(err.message) //Detect push error and
+    })
   )
 
   /**
